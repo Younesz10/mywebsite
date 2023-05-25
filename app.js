b87b1f265b244d7e5fc0c65798a88a251cd67128
younesz10 <yrz.ziewki18@gmail.com>
2023-05-25 17:02:43 -0400

    javascript+menu closes when an option is selected

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menulinks.classList.toggle("active");
 });
+// Close menu when any menu option is selected
+const menuOptions = document.querySelectorAll(".navbar__menu li a");
+
+menuOptions.forEach(function (option) {
+  option.addEventListener("click", function () {
+    menu.classList.remove("is-active");
+    menulinks.classList.remove("active");
+  });
+});
