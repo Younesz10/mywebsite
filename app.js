13def5ab74d522301740095a35ec20fb3cda0292
younesz10 <yrz.ziewki18@gmail.com>
2023-06-02 01:36:31 -0400

    Added an image carousel using javascript

--- app.js
+++ app.js
@@ -14,3 +14,35 @@ menuOptions.forEach(function (option) {
     menulinks.classList.remove("active");
   });
 });
+
+var slideIndex = 0;
+var slides = document
+  .getElementsByClassName("carousel")[0]
+  .getElementsByTagName("img");
+var prevBtn = document.getElementById("prevBtn");
+var nextBtn = document.getElementById("nextBtn");
+
+prevBtn.addEventListener("click", function () {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlide();
+});
+
+nextBtn.addEventListener("click", function () {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  showSlide();
+});
+
+function showSlide() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+
+showSlide();
